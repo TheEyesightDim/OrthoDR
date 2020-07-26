09ce78930735d015d03ead6d75978855426efbb3
Salvador Issa <anisohedron@gmail.com>
2020-07-26 15:06:59 -0400

    added more to AnimationHandler; still unfinished

--- utility/animation_handler.rb
+++ utility/animation_handler.rb
@@ -1,2 +1,32 @@
+# !INCOMPLETE!
+
 class AnimationHandler
+  # Internal class for holding the animation
+  class AnimEntity
+    attr_accessor :entity, :animations
+
+    def initialize(entity, animations)
+      @entity = entity
+      # Even if you pass in a single animation, should still be an Array here
+      @animations = [animations].flatten
+    end
+  end
+
+  def initialize
+    @entity_list = []
+  end
+
+  def register_entity(entity, animations = [])
+    @entity_list << AnimEntity.new(entity, animations)
+  end
+
+  def advance
+    @entity_list.each do |e|
+      e.animations.each { |a| a.call(e) }
+    end
+  end
 end
+
+# What should an animation look like?
+# Animations are Procs (created with Proc.new), or a lambda, with the following signature:
+# fn(entity, active)
